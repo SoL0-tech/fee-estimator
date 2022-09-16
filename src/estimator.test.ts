872f5d0cbc0335b89cb91002095f9716e07dbdc8
Jared Murphy <jlmurphy.sa@gmail.com>
2022-09-16 17:04:58 +0200

    Implement appropriate mocks and test suite for the estimator

---
+++ src/estimator.test.ts
@@ -0,0 +1,48 @@
+import Web3 from 'web3'
+import { FeeEstimator } from './estimator'
+import web3 from './mocks/web3.mock'
+
+let estimator: FeeEstimator
+beforeEach(() => {
+	estimator = new FeeEstimator(web3)
+})
+
+afterEach(() => {
+	web3.reset()
+})
+
+
+test('should return ready after one block', async () => {
+	await sleep(75)
+	const ready = estimator.isReady()
+	expect(ready).toBe(true)
+})
+
+test('should return correct estimate after one block', async () => {
+	await sleep(75)
+	const estimate = estimator.getEstimate()
+	expect(estimate).toBe(20000000000)
+})
+
+test('should return correct estimate after two blocks', async () => {
+	await sleep(110)
+	const estimate = estimator.getEstimate()
+	expect(estimate).toBe(30000000000)
+})
+
+test('should return correct estimate for avg(5) after five blocks', async () => {
+	await sleep(275)
+	const estimate = estimator.getEstimate(5)
+	expect(estimate).toBe(40000000000)
+})
+
+test('should return correct estimate for avg(10) after ten blocks', async () => {
+	await sleep(520)
+	const estimate = estimator.getEstimate(5)
+	expect(estimate).toBe(54000000000)
+})
+
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
---
+++ src/mocks/web3.mock.ts
@@ -0,0 +1,75 @@
+import Web3 from "web3"
+
+let numGetBlockCalls: number
+let subscriptionTimer: NodeJS.Timer
+const INTERVAL_IN_MS = 50
+
+const web3 = {
+	/**
+	 * Helper function to reset state
+	 */
+	reset: () => {
+		numGetBlockCalls = 0
+		clearInterval(subscriptionTimer)
+	},
+	/**
+	 * Mock eth Client API
+	 */
+	eth: {
+
+		/**
+		 * Mock the getBlock operation, and return deterministic test data with each invocation
+		 */
+		getBlock: jest.fn((
+			which: 'latest', includeTransactionData: boolean
+		) => {
+			const vals = [
+				10000000000, 20000000000, 30000000000,
+				40000000000, 50000000000, 60000000000,
+				70000000000, 80000000000, 90000000000,
+				10000000000, 20000000000, 30000000000,
+				40000000000, 50000000000, 60000000000,
+				70000000000, 80000000000, 90000000000,
+				10000000000, 20000000000, 30000000000,
+				40000000000, 50000000000, 60000000000,
+				70000000000, 80000000000, 90000000000,
+			  10000000000, 20000000000, 30000000000,
+			]
+
+			// Next gasPrice value to return
+			const valueToReturn = vals[numGetBlockCalls % vals.length] 
+			// Increment counter for the function
+			numGetBlockCalls += 1
+
+			// Return 10 test transactions with the given gasPrice
+			return {
+				transactions: Array.from({
+					length: 10
+				}, () => ({
+					gasPrice: valueToReturn,
+				}))
+			}
+		}),
+
+		/**
+		 * Mock the subscribe operation, and emit test events at regular intervals
+		 */
+		subscribe: jest.fn((
+			_type: string, callback: (error: Error | null, log: any) => void
+		) => {
+			let n = 1
+			subscriptionTimer = setInterval(() => {
+				callback(null, constructLogObject())
+				n += 1
+			}, INTERVAL_IN_MS)
+
+			function constructLogObject() {
+				return {
+					number: n
+				}
+			}
+		})
+	}
+} as any as Web3 & { reset: Function }
+
+export = web3 
